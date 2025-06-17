79eb25d3873941db6ddf74643be1b681b73471db
Ahmed Alam <61699497+alamahmed@users.noreply.github.com>
2025-06-17 19:01:47 +0200

    Fix home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -122,7 +122,7 @@ const Home = () => {
                 justify={matches ? 'space-between' : 'space-evenly'}
                 gap={matches ? 0 : 'md'}
             >
-                <Card withBorder radius={'lg'} py={matches ? 0 : 'xl'} h={matches ? 'auto' : '60vh'} w={matches ? '49%' : '100%'} p={matches ? 'xl' : 'md'}>
+                <Card withBorder radius={'lg'} py={'xl'} h={matches ? 'auto' : '60vh'} w={matches ? '49%' : '100%'} p={matches ? 'xl' : 'md'}>
                     <Card.Section px={matches ? 'xl' : 'md'} pb={'md'}>
                         <Title order={2}>My Projects</Title>
                     </Card.Section>
